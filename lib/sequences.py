97360e125cf48cef4e9fb595a8dd39de4a857c9a
Jhon Waree Marcelo <jhon.waree.marcelo@gmail.com>
2023-07-13 02:57:34 -0400

    Complete Sequences lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci=[]
+    i=0
+    while i < length:
+        if i == 0:
+            fibonacci.append(i)
+            i+=1
+        elif i == 1:
+            fibonacci.append(i)
+            i+=1
+        else:
+            fibonacci.append(fibonacci[i-2]+fibonacci[i-1])
+            i+=1
+    print(fibonacci)
+    
+
